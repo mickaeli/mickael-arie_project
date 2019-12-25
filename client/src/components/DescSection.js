e212d46de427760c068498a4f70d2e5558f34491
Mickael Israel <mickaelisrael26@gmail.com>
2019-12-25 22:12:25 +0200

    Home page v3

--- client/src/components/DescSection.js
+++ client/src/components/DescSection.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './DescSection.css';
+import Container from 'react-bootstrap/Container'
 
 const DescSection = () => {
   return (
-    <div className='desc-section'>
-      <p>Gooder™ &nbsp;is a new way of good-doing. Expand your community help while connecting & 
-        socializing with people from all over the world. Whether you need something or have something to offer - 
-        Gooder™ &nbsp;will open a new world of opportunities. Make your life better or change someone's life within a second - 
-        It's all up to you.</p>
-    </div>
+    <Container>
+       <div className= 'desc-section'>
+         <p>Gooder™ &nbsp;is a new way of good-doing. Expand your community help while connecting & 
+           socializing with people from all over the world. Whether you need something or have something to offer - 
+           Gooder™ &nbsp;will open a new world of opportunities. Make your life better or change someone's life within a second - 
+           It's all up to you.</p>
+       </div>
+    </Container>  
   );
 };
 
--- client/src/components/DescSection2.js
+++ client/src/components/DescSection2.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './DescSection2.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import under_gallerie from '../img/underground_gallery.jpg';
 
 const DescSection2 = () => {
   return (
+    <Container>
       <div className='desc-section2'>
-        <img src={under_gallerie} alt="underground gallery"/>
-        <div className='container'>
-          <h1>So.. who we are and what are we all about?</h1>
-          <p>Gooder™ is a social network for good-doing & good causes. 
-            Whether you need help, have something to offer or run a charity organization - 
-            Gooder™ is the place for you. After logging in, you'll be able to build a personal 
-            or organizational profile page, connect with others, socialize and make good. 
-            Gooder™ is the best place for anyone interested in asking for or doing good actions, 
-            gathering around good causes, getting to know people & help. </p>
-            <p>Gooder™ were made as a final project at Hadassah Academic College, Computer Science department.</p>
-          </div>
-      </div>
+          <Row>
+            <Col md={12} lg={6}>
+              <img src={under_gallerie} alt="underground gallery"/>
+            </Col>
+            <Col md={12} lg={6}>
+              <div className='container-wrapper'>
+                <h1>So.. who we are and what are we all about?</h1>
+                <p>Gooder™ is a social network for good-doing & good causes. 
+                  Whether you need help, have something to offer or run a charity organization - 
+                  Gooder™ is the place for you. After logging in, you'll be able to build a personal 
+                  or organizational profile page, connect with others, socialize and make good. 
+                  Gooder™ is the best place for anyone interested in asking for or doing good actions, 
+                  gathering around good causes, getting to know people & help. </p>
+                <p>Gooder™ were made as a final project at Hadassah Academic College, Computer Science department.</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
   );
 };
 

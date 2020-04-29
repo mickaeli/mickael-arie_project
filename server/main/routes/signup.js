093459036471c647b0323ce0706ba90b18f2618e
Arie Noyoz <arie.noyoz@mobileye.com>
2020-04-29 22:59:57 +0300

    Sequelize v3

--- server/main/routes/signup.js
+++ server/main/routes/signup.js
@@ -1,32 +1,18 @@
 var express = require('express')
 var Sequelize = require('sequelize')
 var router = express.Router()
-var async = require('async')
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
-})
 
 const User = require('../models/user');
 const UserDetails = require('../models/userDetails');
 
-async.waterfall(
-	[
-	 function(callback) {
-		User.sync()
-		 .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-		 .catch(error => console.log('This error occured', error));
-		 callback();
-	 },
-	 function(callback) {
-
-		UserDetails.sync()
-		 .then(() => console.log('usersDetails table has been successfully created, if one doesn\'t exist'))
-		 .catch(error => console.log('This error occured', error));
-	 }
-	]);
-
+User.sync()
+              .then(() => {
+			 console.log('users table has been successfully created, if one doesn\'t exist')
+			 UserDetails.sync()
+                                 .then(() => console.log('usersDetails table has been successfully created, if one doesn\'t exist'))
+                                 .catch(error => console.log('This error occured', error));
+	      })
+              .catch(error => console.log('This error occured', error))
 
 const validation = require('../utils/validate')
 

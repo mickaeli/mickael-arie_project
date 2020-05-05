811d5004a734f67da2fd2f7f655a9dc2359047bb
Mickael Israel <mickaelisrael26@gmail.com>
2020-05-05 19:36:48 +0300

    wall v6 - post edition added

---
+++ server/main/models/db_sync.js
@@ -0,0 +1,29 @@
+const User = require('./user');
+const UserDetails = require('./userDetails');
+const Post = require('./post');
+
+module.exports = (function() {
+  var that = {
+    create_tables: function() {
+      // create db tables
+
+      //create users table
+      User.sync()
+      .then(() => {
+        console.log('users table has been successfully created, if one doesn\'t exist')
+        //create users_details table
+        UserDetails.sync()
+        .then(() => {
+          console.log('users_details table has been successfully created, if one doesn\'t exist')
+          //create posts table
+          Post.sync()
+          .then(() => console.log('posts table has been successfully created, if one doesn\'t exist'))
+          .catch(error => console.log('This error occured', error))})
+        .catch(error => console.log('This error occured', error));
+
+    })
+    .catch(error => console.log('This error occured', error));
+  }
+}
+return that;
+})(); 

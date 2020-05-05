a7bc595f7a361efa2a54da9d3f87d046dc50d8e8
Arie Noyoz <arie.noyoz@mobileye.com>
2020-05-05 14:32:26 +0300

    spacing

--- server/main/models/db_sync.js
+++ server/main/models/db_sync.js
@@ -5,22 +5,22 @@ const Post = require('./post');
 module.exports = (function() {
    var that = {
       create_tables: function() {
-// create db tables
-User.sync()
-.then(() => {
-        console.log('users table has been successfully created, if one doesn\'t exist')
-        // create users_details table
-        UserDetails.sync()
-        .then(() => {
-		console.log('users_details table has been successfully created, if one doesn\'t exist')
-		Post.sync()
-.then(() => console.log('posts table has been successfully created, if one doesn\'t exist'))
-.catch(error => console.log('This error occured', error))})
-        .catch(error => console.log('This error occured', error));
+	// create db tables
+	User.sync()
+	.then(() => {
+        	console.log('users table has been successfully created, if one doesn\'t exist')
+        	// create users_details table
+        	UserDetails.sync()
+        	.then(() => {
+			console.log('users_details table has been successfully created, if one doesn\'t exist')
+			Post.sync()
+			.then(() => console.log('posts table has been successfully created, if one doesn\'t exist'))
+			.catch(error => console.log('This error occured', error))})
+        	.catch(error => console.log('This error occured', error));
 
-})
-.catch(error => console.log('This error occured', error));
-}
-}
-return that;
+	})
+	.catch(error => console.log('This error occured', error));
+      }
+  }
+  return that;
 })();

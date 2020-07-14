3f00c47d19ef0a86263343b7aa8203e889391c07
Arie Noyoz <arie.noyoz@mobileye.com>
2020-07-14 17:44:31 +0300

    heroku deploy

--- server/main/app.js
+++ server/main/app.js
@@ -25,16 +25,16 @@ const friendsRouter = require('./routes/friends')
 
 var app = express();
 
-//if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  //app.use(express.static(__dirname));
+  app.use(express.static(__dirname, '../client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   //const path = require('path');
   //app.get('*', (req, res) => {
      //res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'));
   //});
-//}
+}
 
 var bodyParser = require('body-parser');
 

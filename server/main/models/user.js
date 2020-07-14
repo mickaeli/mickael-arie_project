8b4a66ef5f02eb8bb8e317e8a6e5f695d394a76d
Arie Noyoz <arie.noyoz@mobileye.com>
2020-07-14 16:21:57 +0300

    minor modification

--- server/main/models/user.js
+++ server/main/models/user.js
@@ -4,7 +4,7 @@ const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../.env')})
 
 // create a sequelize instance with our local postgres database information.
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres'
 });
